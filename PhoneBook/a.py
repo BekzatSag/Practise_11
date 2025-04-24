fbc46a207acc1cfc576e4fdd1b5a516ed2f794a5
BekzatSag <bsagimgeldiev@gmail.com>
2025-04-24 10:28:58 +0500

    new_features

--- PhoneBook/a.py
+++ PhoneBook/a.py
@@ -1,6 +1,6 @@
 import csv
 
-data = [["Names", "Phone"], ["z", 87772491653], ["Nurik", 87051442666], ["A", 87772491653], ["B", 87772491653], ["C", 87772491653], ["a", 22222222222], ["b", 11111111111]]
+data = [["Names", "Phone"], ["Bekzat", 87772491653], ["Nurik", 87051442666], ["A", 11111111111], ["B", 22222222222]]
 with open("PhoneBook.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(data)
--- PhoneBook/b.py
+++
@@ -1,4 +0,0 @@
-name_phone = [("Bekxat", 2882), ("ZZ", 88)].sort(key = lambda x: x[0])
-
-print(name_phone)
-    
